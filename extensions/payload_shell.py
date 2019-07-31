afd948dceeed50e9b43f01237ae014e5330248e3
Nicolas Cosnard <nicolas@cosnard.io>
2019-07-31 18:36:37 +0200

    Added shell payload

---
+++ extensions/payload_shell.py
@@ -0,0 +1,26 @@
+class ExtStr(type):
+    def __str__(self):
+        return self.getKey()
+
+class BaboosshExt(object,metaclass=ExtStr):
+    @classmethod
+    def getModType(cls):
+        return "payload"
+
+    @classmethod
+    def getKey(cls):
+        return "shell"
+
+    @classmethod
+    def descr(cls):
+        return "Get a shell on target"
+    
+    @classmethod
+    def run(cls,socket, connection):
+        try:
+            socket.run("sh",pty=True)
+        except Exception as e:
+            print("Error : "+str(e))
+            return False
+        return True
+    
