78acfddeb38f3c22c69451e67876f30e0d41f40e
Niko <nicolas@cosnard.io>
2020-01-19 11:05:40 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="baboossh",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_namespace_packages(include=['baboossh*']),
     scripts=['bin/baboossh'],
 
