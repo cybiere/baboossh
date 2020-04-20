999ea311b1b60f5e76a54b449100a2e8cc564dde
Niko <nicolas@cosnard.io>
2020-04-20 13:11:14 +0200

    Added tunnel doc

--- baboossh/tunnel.py
+++ baboossh/tunnel.py
@@ -4,8 +4,32 @@ import asyncio, asyncssh, sys
 import multiprocessing as mp
 
 class Tunnel():
+    """Open SOCKS tunnel to a host to proxify other tools
+
+    Each tunnel is opened in another process so that BabooSSH is still usable
+    while the tunnel is open.
+
+    Attributes:
+        connection (:class:`.Connection`): the tunnel exit
+        port (int): the tunnel entrance (local) port. Uses a random port if
+            none is provided.
+        socket (asyncssh.SSHClientConnection): the connection to the exit once
+            it is opened.
+    """
 
     async def listenAndLock(c,port,q):
+        """Open the proxy on the connection and wait.
+
+        Once the connection is opened, this function opens the proxy and waits.
+        The success is returned to the main process as a boolean through a Queue,
+        with the error message in case of failure.
+
+        Args:
+            c (asyncssh.SSHClientConnection): the opened connection to the exit
+            port (int): the local entrance port
+            q (multiprocessing.Queue): the queue to report status to the main process
+        """
+
         try:
             listener = await c.forward_socks('localhost',port)
         except Exception as e:
@@ -15,6 +39,12 @@ class Tunnel():
         await listener.wait_closed()
 
     def proxLock(c,port,q):
+        """Run :func:`~tunnel.Tunnel.listenAndLock`
+
+        Args:
+            See :func:`~tunnel.Tunnel.listenAndLock`
+        """
+
         asyncio.get_event_loop().run_until_complete(Tunnel.listenAndLock(c,port,q))
 
     def __init__(self,connection,port=None):
@@ -36,13 +66,19 @@ class Tunnel():
         print("Tunnel to "+str(self.connection)+" open on port "+str(self.port))
 
     def getPort(self):
+        """Get the tunnel port
+
+        Returns:
+            The tunnel entrance port (int)
+        """
+
         return self.port
 
     def close(self):
+        """Close a previously opened port"""
         self.subprocess.terminate()
         self.socket.close()
         print("Tunnel port "+str(self.port)+" closed")
 
     def __str__(self):
         return str(self.port)+"->"+str(self.connection)
-
